a258710a7549ff120627794cf187880d063eaea1
SAI0969 <asm1scs590526@gmail.com>
2026-07-27 14:56:26 +0530

    Create ipl_without_2v.py

---
+++ ipl_without_2v.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("ipl2013.csv")
+
+numeric = df.select_dtypes(include=np.number)
+
+X = numeric.drop(columns=["SOLD PRICE"]).values
+y = numeric["SOLD PRICE"].values
+
+weights = np.zeros(X.shape[1])
+bias = 0
+
+learning_rate = 0.0000000001
+epochs = 4
+
+n = len(X)
+
+for epoch in range(epochs):
+
+    y_pred = np.dot(X, weights) + bias
+
+    error = y_pred - y
+
+    dw = (2 / n) * np.dot(X.T, error)
+    db = (2 / n) * np.sum(error)
+
+    weights = weights - learning_rate * dw
+    bias = bias - learning_rate * db
+
+print("Final Weights:", weights)
+print("Final Bias:", bias)
