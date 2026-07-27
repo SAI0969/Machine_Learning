48fa949defab6fbf4a4ea6e656bf2e7a68d05e88
SAI0969 <asm1scs590526@gmail.com>
2026-07-27 15:03:52 +0530

    Update ipl_without_1v.py

--- ipl_without_1v.py
+++ ipl_without_1v.py
@@ -5,17 +5,18 @@ df = pd.read_csv("ipl2013.csv")
 
 numeric = df.select_dtypes(include=np.number)
 
-
 X = numeric.drop(columns=["SOLD PRICE"]).values
 y = numeric["SOLD PRICE"].values
 
 weights = np.zeros(X.shape[1])
 bias = 0
 
-learning_rate = 0.01
+learning_rate = 0.0000000001
 epochs = 4
 
 n = len(X)
+
+best_rmse = float("inf")
 
 for epoch in range(epochs):
 
@@ -26,14 +27,19 @@ for epoch in range(epochs):
     dw = (2 / n) * np.dot(X.T, error)
     db = (2 / n) * np.sum(error)
 
-
     weights = weights - learning_rate * dw
     bias = bias - learning_rate * db
 
-    print("Iteration:", epoch + 1)
-    print("Weights:", weights)
-    print("Bias:", bias)
-    print()
+    y_pred_new = np.dot(X, weights) + bias
 
-print("Final Weights:", weights)
+    rmse = np.sqrt(np.mean((y - y_pred_new) ** 2))
+
+    print("Iteration:", epoch + 1)
+    print("RMSE:", rmse)
+
+    if rmse < best_rmse:
+        best_rmse = rmse
+
+print("\nFinal Weights:", weights)
 print("Final Bias:", bias)
+print("Reduced RMSE:", best_rmse)
